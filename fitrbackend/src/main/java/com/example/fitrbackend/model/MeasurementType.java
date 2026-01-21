6c3a4fa91d25c8e49951d3da5fa09e497bf8c13f
Ambrose Blay <ab2838@cornell.edu>
2026-01-21 12:40:16 -0500

    feat: add MeasurementType enum for exercise tracking
    Introduce MeasurementType enum to define supported measurement units
    for exercises. This enum includes REPS, TIME, REPS_AND_TIME,
    TIME_AND_WEIGHT, REPS_AND_WEIGHT, DISTANCE_AND_TIME, and
    CALORIES_AND_TIME to support flexible exercise logging capabilities.

---
+++ fitrbackend/src/main/java/com/example/fitrbackend/model/MeasurementType.java
@@ -0,0 +1,11 @@
+package com.example.fitrbackend.model;
+
+public enum MeasurementType {
+    REPS,
+    TIME,
+    REPS_AND_TIME,
+    TIME_AND_WEIGHT,
+    REPS_AND_WEIGHT,
+    DISTANCE_AND_TIME,
+    CALORIES_AND_TIME
+}
