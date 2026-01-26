e8683803d68519ad05f3fc73b75d0007fdb6703f
Ambrose Blay <ab2838@cornell.edu>
2026-01-25 20:34:50 -0500

    feat: add Gender enum and UserProfileService with profile retrieval
    Add Gender enum with MALE, FEMALE, OTHER values.
    
    Implement UserProfileService to fetch user profiles by ID,
    including user details and profile data conversion to
    UserProfileResponse.

---
+++ fitrbackend/src/main/java/com/example/fitrbackend/service/UserProfileService.java
@@ -0,0 +1,51 @@
+package com.example.fitrbackend.service;
+
+import com.example.fitrbackend.dto.UserProfileResponse;
+import com.example.fitrbackend.exception.DataNotFoundException;
+import com.example.fitrbackend.model.User;
+import com.example.fitrbackend.model.UserProfile;
+import com.example.fitrbackend.repository.UserProfileRepository;
+import com.example.fitrbackend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProfileService {
+
+    private final UserProfileRepository profileRepo;
+
+    private final UserRepository userRepo;
+
+    public UserProfileService(UserProfileRepository profileRepo, UserRepository userRepo) {
+        this.profileRepo = profileRepo;
+        this.userRepo = userRepo;
+    }
+
+    public UserProfileResponse getUserProfile(Long id) {
+        System.out.println("getting profile");
+        User user = userRepo.findById((id)).orElseThrow(() -> new DataNotFoundException(id, "user"));
+        UserProfile profile = profileRepo.findByUser(user);
+        if (profile == null) {
+            throw new DataNotFoundException(id, "userProfile");
+        }
+        return toUserProfileResponse(profileRepo.findByUser(user));
+    }
+
+    private UserProfileResponse toUserProfileResponse(UserProfile profile) {
+        return new UserProfileResponse(
+                profile.getId(),
+                profile.getUser().getId(),
+                profile.getUser().getFirstname(),
+                profile.getUser().getLastname(),
+                profile.getUser().getEmail(),
+                profile.getGender(),
+                profile.getHeight(),
+                profile.getWeight(),
+                profile.getExperienceLevel(),
+                profile.getGoal(),
+                profile.getPreferredWeightUnit(),
+                profile.getPreferredDistanceUnit(),
+                profile.getCreatedAt()
+
+        );
+    }
+}
