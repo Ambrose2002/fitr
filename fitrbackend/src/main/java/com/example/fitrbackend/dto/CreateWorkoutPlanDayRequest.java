0fa9c56e4fb736a54427682fb1969351a2435e55
Ambrose Blay <ab2838@cornell.edu>
2026-02-27 22:34:26 -0500

    feat: add Jackson annotations for CreateWorkoutPlanDayRequest JSON mapping
    Add @JsonProperty("day_number") and @JsonAlias("dayNumber") to dayNumber field
    Add @JsonProperty("name") to name field

--- fitrbackend/src/main/java/com/example/fitrbackend/dto/CreateWorkoutPlanDayRequest.java
+++ fitrbackend/src/main/java/com/example/fitrbackend/dto/CreateWorkoutPlanDayRequest.java
@@ -1,9 +1,15 @@
 package com.example.fitrbackend.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class CreateWorkoutPlanDayRequest {
+    @JsonProperty("day_number")
+    @JsonAlias("dayNumber")
     private int dayNumber;
+
+    @JsonProperty("name")
     private String name;
 }
